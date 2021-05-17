f4245201e3f80da90a8d57d290229699ff2a3b54
gauthier-th <mail@gauthier-thomas.dev>
2021-05-17 19:18:36 +0200

    Switch to totem from Divia website

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const fetch = require('isomorphic-unfetch');
-const jwt_decode = require('jwt-decode');
 const Line = require('./Line');
 const Stop = require('./Stop');
-
-const baseURL = 'https://tim.divia.fr/api/';
 
 class DiviaAPI {
 
 	constructor() {
 		/** @type {object} */
 		this.reseau = null;
-		this.baseURL = baseURL;
 		/** @type {string} */
 		this._token = null;
 	}
@@ -66,25 +62,6 @@ class DiviaAPI {
 	 */
 	get stops() {
 		return Object.values(this.reseau.arrets);
-	}
-
-	/**
-	 * @param {string} username 
-	 * @param {string} password 
-	 */
-	async _getToken(username, password) {
-		if (!this._token || jwt_decode(this._token).exp <= Math.round(Date.now() / 1000)) {
-			const response = await fetch(`${baseURL}login_check`, {
-				method: 'POST',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/x-www-form-urlencoded'
-				},
-				body: `_username=${username}&_password=${password}`
-			}).then(res => res.json());
-			this._token = response.token;
-		}
-		return this._token;
 	}
 
 }
