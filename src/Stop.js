8ea4a1c711dbf8696773485cf1799866e8d95a7b
gauthier-th <mail@gauthier-thomas.dev>
2021-05-21 15:45:20 +0200

    Fix totem request

--- src/Stop.js
+++ src/Stop.js
@@ -42,7 +42,7 @@ class Stop {
 	 */
 	async totem(username, password) {
 		const token = await this.api._getToken(username, password);
-		const response = await fetch(`${this.api.baseURL}get/totem?source_type=bo_divia_utilisateur&source_uuid=${uuidv4()}&source_id=&ligne=${this.line.data.id}&arret=${this.data.id}&token=${token}`).then(res => res.json());
+		const response = await fetch(`${this.api.baseURL}get/totem?source_type=bo_divia_utilisateur&source_uuid=${uuidv4()}&source_id=&ligne=${this.line.data.id}&arret=${this.data.code}&token=${token}`).then(res => res.json());
 		return response.result_infos.totem;
 	}
 
