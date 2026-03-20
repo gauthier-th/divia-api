7dda6191e1b5f64f50de67f1798ed4b9a3b913a3
gauthier-th <mail@gauthierth.fr>
2026-03-20 15:07:24 +0100

    Fix datetime timezone

--- src/formatters.ts
+++ src/formatters.ts
@@ -1,23 +1,5 @@
 import type { DateTimeObject, DirectionObject, LineObject, RouteObject, ScheduleObject, StopAreaObject, StopPointObject } from "./types";
 
-const timezone = "Europe/Paris";
-
-function getTimezoneOffset(timeZone: string): number {
-  const now = new Date();
-  const tz = new Intl.DateTimeFormat('en-US', {
-    timeZone,
-    timeZoneName: 'longOffset'
-  }).formatToParts(now);
-
-  const offset = tz.find(p => p.type === 'timeZoneName')!.value;
-
-  const match = offset.match(/GMT([+-]\d{2}):(\d{2})/)!;
-  const hours = parseInt(match[1], 10);
-  const minutes = parseInt(match[2], 10);
-
-  return hours * 60 + Math.sign(hours) * minutes;
-}
-
 function formatTimeString(time: string): Date { // "HHMMSS"
   const hours = parseInt(time.substring(0, 2), 10);
   const minutes = parseInt(time.substring(2, 4), 10);
@@ -34,8 +16,7 @@ function formatDateTimeString(dateTime: string): Date { // "YYYYMMDDTHHMMSS"
   const hours = parseInt(dateTime.substring(9, 11), 10);
   const minutes = parseInt(dateTime.substring(11, 13), 10);
   const seconds = parseInt(dateTime.substring(13, 15), 10);
-  const date = new Date(Date.UTC(year, month, day, hours, minutes, seconds));
-  return new Date(date.getTime() - getTimezoneOffset(timezone) * 60000);
+  return new Date(Date.UTC(year, month, day, hours, minutes, seconds));
 }
 
 export function formatLine(line: any): LineObject {
